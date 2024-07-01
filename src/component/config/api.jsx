59cd9b35b0a96585b509bf56029998023f857138
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2024-07-01 18:23:47 +0530

    add host url

--- src/component/config/api.jsx
+++ src/component/config/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = "http://localhost:8080"
+export const API_URL = "e-commerce-server-production-6c47.up.railway.app"
 
 export const api = axios.create({
 
