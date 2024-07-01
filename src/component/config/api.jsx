3cd5aaa94ebd588cbc3aae4d94faec1f935dad60
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2024-07-01 18:49:05 +0530

    d

--- src/component/config/api.jsx
+++ src/component/config/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = "e-commerce-server-production-30e0.up.railway.app"
+export const API_URL = "https://e-commerce-server-production-30e0.up.railway.app"
 
 export const api = axios.create({
 
